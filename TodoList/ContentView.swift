6e15141d2df1bf3920c35eaef684b98abc1e4d83
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-11 19:19:23 +0900

    Initial Commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by 강치우 on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 강치우 on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
